8d255656419be2f82bdd83cb4b3a62b996e0774d
samdairy <samdairy@yahoo.com>
2023-05-27 13:05:01 -0500

    first

---
+++ car_inventory/src/firebaseConfig.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyC5IJ1bk4sC1XV7p1uTHLLv_O2ZMu7hQrY",
+    authDomain: "samcar-4835e.firebaseapp.com",
+    projectId: "samcar-4835e",
+    storageBucket: "samcar-4835e.appspot.com",
+    messagingSenderId: "581644361041",
+    appId: "1:581644361041:web:0dbb2ed4bc2e34bbebc770"
+  };
--- car_inventory/src/index.tsx
+++ car_inventory/src/index.tsx
@@ -1,11 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Home, Dashboard, SignIn} from './component';
+import { theme } from './Theme/themes';
+import { ThemeProvider } from '@mui/material/styles';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+import './styles.css';
+import { FirebaseAppProvider } from 'reactfire';
+import 'firebase/auth'
+import { firebaseConfig } from './firebaseConfig'
+
+
 
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+root.render(
+  <React.StrictMode>
+    <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <Routes>
+            <Route path='/' element={<Home title={'Drone Inventory'} />} />
+            <Route path='/dashboard' element={<Dashboard />} />
+            <Route path='/signin' element={<SignIn />} />
+            
+          </Routes>
+        </Router>
+      </ThemeProvider>
+    </Provider>
+    </FirebaseAppProvider>
+  </React.StrictMode>
+);
+
 
 
 
